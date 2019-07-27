a0f52b86d3f34b42815f292284e31cf61504cff6
zj <zj@cnbisoft.com>
2019-07-27 15:43:10 +0800

    注释掉暂时不需要的代码，  调用上传头像的接口

--- src/views/system/Console.vue
+++ src/views/system/Console.vue
@@ -1,38 +1,49 @@
 <template>
-    <!-- <h1>控制台 Console</h1> -->
+  <!-- <h1>控制台 Console</h1> -->
   <div class="main">
     <div class="main1">
       <div class="body_one" style="height:280px;">
         <div class="body_one_head">
-          <a-icon class="lanyuan"/>
+          <a-icon class="lanyuan" />
           {{userName}}
           <span class="anse">欢迎使用，您现在所在的是经邦企业系统管理后台。</span>
         </div>
         <div class="body_one_down">
           <div class="body_one_down_left" style="padding-left:200px;padding-top:55px;">
-              
-              <div class="body_one_down_left_head_down_left">
-                <p class="body_one_down_left_head" style="color:rgba(42,43,47,1);">账号剩余</p>
-                <p>
-                  <a-icon class="lanzheng" style="height:16px;width:16px;background:rgba(77,124,254,1);"/>
-                  剩余可用账户{{shengyu}}个
-                </p>
-                <p>
-                  <a-icon class="huizheng" style="height:16px;width:16px;background:rgba(163,160,251,1);"/>
-                  已开通使用数{{alreadyUseNum}}个
-                </p>
-                <p>
-                  <a-icon class="zheng" style="height:16px;width:16px;border:2px solid rgba(177,179,183,1);opacity:0.3;"/>
-                  可用账户总数{{Total}}个
-                </p>
-              </div>
-              <div class="body_one_down_left_head_down_right" style="padding-top:35px;">
-                <a-progress type="circle" :percent="this.rate" :format="() => ` 剩余可用${shengyu}个`"/>
-              </div>
+            <div class="body_one_down_left_head_down_left">
+              <p class="body_one_down_left_head" style="color:rgba(42,43,47,1);">账号剩余</p>
+              <p>
+                <a-icon
+                  class="lanzheng"
+                  style="height:16px;width:16px;background:rgba(77,124,254,1);"
+                />
+                剩余可用账户{{shengyu}}个
+              </p>
+              <p>
+                <a-icon
+                  class="huizheng"
+                  style="height:16px;width:16px;background:rgba(163,160,251,1);"
+                />
+                已开通使用数{{alreadyUseNum}}个
+              </p>
+              <p>
+                <a-icon
+                  class="zheng"
+                  style="height:16px;width:16px;border:2px solid rgba(177,179,183,1);opacity:0.3;"
+                />
+                可用账户总数{{Total}}个
+              </p>
+            </div>
+            <div class="body_one_down_left_head_down_right" style="padding-top:35px;">
+              <a-progress type="circle" :percent="this.rate" :format="() => ` 剩余可用${shengyu}个`" />
+            </div>
           </div>
           <div class="body_one_down_right" style="padding-left:500px;padding-top:55px;">
             <p class="body_one_down_right_head" style="color:rgba(42,43,47,1);">当前版本</p>
-            <p class="body_one_down_right_head" style="color:rgba(42,43,47,1);">您当前使用的是<span style="color:rgba(77,124,254,1);font-size:22px;">{{VerName}}</span></p>
+            <p class="body_one_down_right_head" style="color:rgba(42,43,47,1);">
+              您当前使用的是
+              <span style="color:rgba(77,124,254,1);font-size:22px;">{{VerName}}</span>
+            </p>
             <p class="anse">免费版团队人数上限为{{Total}}人，若有扩容需要，请选择购买</p>
             <div>
               <a-button type="primary" @click="buy">立即购买</a-button>
@@ -43,24 +54,28 @@
       <div class="body_two" style="height:320px;">
         <div class="body_one_down_left">
           <div>
-            <p class="body_two_head_left" style="font-size:22px;">应用管理<span class="anse" style="font-size:14px;">&nbsp;&nbsp;&nbsp;&nbsp;企业已启用了{{applyCount}}个应用</span></p>
+            <p class="body_two_head_left" style="font-size:22px;">
+              应用管理
+              <span
+                class="anse"
+                style="font-size:14px;"
+              >&nbsp;&nbsp;&nbsp;&nbsp;企业已启用了{{applyCount}}个应用</span>
+            </p>
           </div>
           <div class="body_two_head_right" style="padding-left:1250px;">
             <a-button type="primary">+ 开启应用</a-button>
           </div>
         </div>
         <div class="body_two_down">
-          <div class="models" 
-              v-for="(item,index) of newList"
-              :key="index">
-              <div class="model_one">
-                <!-- {{item.img}}   -->
-                图片暂无
-              </div>  
-              <div class="model_two">
-                <p>{{item.name}}</p>
-                  {{item.note}}
-              </div>
+          <div class="models" v-for="(item,index) of newList" :key="index">
+            <div class="model_one">
+              <!-- {{item.img}}   -->
+              图片暂无
+            </div>
+            <div class="model_two">
+              <p>{{item.name}}</p>
+              {{item.note}}
+            </div>
           </div>
         </div>
       </div>
@@ -68,15 +83,9 @@
         <div class="footer_head">
           <p style="font-size:22px;">客户端</p>
         </div>
-        <div class="models2"
-            v-for="(item,index) of footList"
-            :key="index">
-            <div class="height-50">
-                {{item.img}}  
-            </div>  
-            <div>
-              {{item.text}}
-            </div>
+        <div class="models2" v-for="(item,index) of footList" :key="index">
+          <div class="height-50">{{item.img}}</div>
+          <div>{{item.text}}</div>
         </div>
       </div>
     </div>
@@ -84,166 +93,166 @@
 </template>
 
 <script>
-import { getUserInfo , getLicense ,findUserCount ,getCurApp ,getDisableAppId} from '@/api/mylogin'
-  export default {
-    name: 'Console',
-    data() {
-      return {
-         newList:[],
-         Total:0,//账户总数
-         alreadyUseNum:0, //已使用数量
-         userName:'',//当前用户的name
-         VerName:'',//版本name(免费/旗舰)
-         applyTotalCount:0,//应用的总数
-         disableAppleCount:0,//被禁用的数量
-         list:[],//应用的数组
-         list1:[],//被禁用的id数组
-         //控制台最底部的内容数组
-         footList:[
-           {
-             img:'图1',
-             text:'进入web版'
-           },
-           {
-             img:'图2',
-             text:'iPhone应用'
-           },
-           {
-             img:'图3',
-             text:'Android应用'
-           },
-           {
-             img:'图4',
-             text:'macOS客户端'
-           },
-           {
-             img:'图5',
-             text:'Windows客户端'
-           }
-         ] 
-      }
+import { getUserInfo, getLicense, findUserCount, getCurApp, getDisableAppId } from '@/api/mylogin'
+export default {
+  name: 'Console',
+  data () {
+    return {
+      newList: [],
+      Total: 0,//账户总数
+      alreadyUseNum: 0, //已使用数量
+      userName: '',//当前用户的name
+      VerName: '',//版本name(免费/旗舰)
+      applyTotalCount: 0,//应用的总数
+      disableAppleCount: 0,//被禁用的数量
+      list: [],//应用的数组
+      list1: [],//被禁用的id数组
+      //控制台最底部的内容数组
+      footList: [
+        {
+          img: '图1',
+          text: '进入web版'
+        },
+        {
+          img: '图2',
+          text: 'iPhone应用'
+        },
+        {
+          img: '图3',
+          text: 'Android应用'
+        },
+        {
+          img: '图4',
+          text: 'macOS客户端'
+        },
+        {
+          img: '图5',
+          text: 'Windows客户端'
+        }
+      ]
+    }
+  },
+  created () {
+    this.getUser_Info();
+    this.get_License();
+    this.find_User_Count();
+    this.get_Cur_App();
+    this.get_Disable_App_Id();
+  },
+  computed: {
+    //剩余账户数量
+    shengyu: function () {
+      return this.Total - this.alreadyUseNum
     },
-    created() {
-      this.getUser_Info();
-      this.get_License();
-      this.find_User_Count();
-      this.get_Cur_App();
-      this.get_Disable_App_Id();
+    //已用账户数量占总账户数的比例
+    rate: function () {
+      return this.alreadyUseNum / this.Total
     },
-    computed: {
-      //剩余账户数量
-      shengyu: function () {
-        return this.Total - this.alreadyUseNum
-      },
-      //已用账户数量占总账户数的比例
-      rate: function(){
-        return this.alreadyUseNum/this.Total
-      },
-      //企业已启用的应用数量
-      applyCount:function(){
-        return this.applyTotalCount-this.disableAppleCount
-      }
+    //企业已启用的应用数量
+    applyCount: function () {
+      return this.applyTotalCount - this.disableAppleCount
+    }
 
 
+  },
+  methods: {
+    buy () {
+      alert('老板，现金还是刷卡')
     },
-    methods: {
-      buy(){
-        alert('老板，现金还是刷卡')
-      },
-      //查询当前的用户信息
-      getUser_Info(){
-        let me = this ;
-        getUserInfo().then(function (res) {
-            // debugger
-            if (res.code === 200 && res.data) {
-              me.userName = res.data.trueName
-            }
-        })
-      },
-      //获取租户的牌照信息
-      get_License(){
-        let me = this ;
-        getLicense().then(function (res) {
-            // debugger
-            if (res.code === 200 && res.data) {
-              me.Total = res.data.maxUser;
-              me.VerName = res.data.name
-            } 
-        })
-      },
-      //获取已使用的租户数量
-      find_User_Count(){
-        let me = this ;
-        findUserCount().then(function (res) {
-            // debugger
-            if (res.code === 200 && res.data) {
-              me.alreadyUseNum = res.data;
-            }
-        })
-      },
-      //获取租户的应用管理
-      get_Cur_App(){
-        let me = this ;
-        getCurApp().then(function (res) {
-            debugger
-            if (res.code === 200 && res.data) {
-              me.list = res.data;
-              me.applyTotalCount = res.data.length;
-              //遍历数组
-              let newList = [];
-              me.list1.forEach(obj1 => {
-                let tempList=me.list.filter(obj=>obj1!==obj.id)
-                newList=[...tempList,...newList]
-              });
-              me.newList=newList
-            } 
-        })
-      },
-      //获取应用管理被禁用的应用
-      get_Disable_App_Id(){
-        let me = this ;
-        getDisableAppId().then(function (res) {
-          debugger
-          if (res.code === 200 && res.data) {
-            me.list1 = res.data;
-            me.disableAppleCount = res.data.length;
-          }
-        })
-      }
-      
+    //查询当前的用户信息
+    getUser_Info () {
+      let me = this;
+      getUserInfo().then(function (res) {
+        // debugger
+        if (res.code === 200 && res.data) {
+          me.userName = res.data.trueName
+        }
+      })
     },
-  }
+    //获取租户的牌照信息
+    get_License () {
+      let me = this;
+      getLicense().then(function (res) {
+        // debugger
+        if (res.code === 200 && res.data) {
+          me.Total = res.data.maxUser;
+          me.VerName = res.data.name
+        }
+      })
+    },
+    //获取已使用的租户数量
+    find_User_Count () {
+      let me = this;
+      findUserCount().then(function (res) {
+        // debugger
+        if (res.code === 200 && res.data) {
+          me.alreadyUseNum = res.data;
+        }
+      })
+    },
+    //获取租户的应用管理
+    get_Cur_App () {
+      let me = this;
+      getCurApp().then(function (res) {
+        debugger
+        if (res.code === 200 && res.data) {
+          me.list = res.data;
+          me.applyTotalCount = res.data.length;
+          //遍历数组
+          let newList = [];
+          me.list1.forEach(obj1 => {
+            let tempList = me.list.filter(obj => obj1 !== obj.id)
+            newList = [...tempList, ...newList]
+          });
+          me.newList = newList
+        }
+      })
+    },
+    //获取应用管理被禁用的应用
+    get_Disable_App_Id () {
+      let me = this;
+      getDisableAppId().then(function (res) {
+        debugger
+        if (res.code === 200 && res.data) {
+          me.list1 = res.data;
+          me.disableAppleCount = res.data.length;
+        }
+      })
+    }
+
+  },
+}
 </script>
 
 <style scoped>
-.head{
-  width:66px;
-  height:29px;
-  font-size:22px;
-  font-family:Microsoft YaHei;
-  font-weight:400;
-  line-height:22px;
-  color:rgba(42,43,47,1);
-  opacity:1;
+.head {
+  width: 66px;
+  height: 29px;
+  font-size: 22px;
+  font-family: Microsoft YaHei;
+  font-weight: 400;
+  line-height: 22px;
+  color: rgba(42, 43, 47, 1);
+  opacity: 1;
 }
-.anse{
-  color:rgba(177,179,183,1);
+.anse {
+  color: rgba(177, 179, 183, 1);
 }
-.lanyuan{
-  width:20px;
-  height:20px;
+.lanyuan {
+  width: 20px;
+  height: 20px;
   line-height: 20px;
-  background:rgba(77,124,254,1);
-  border-radius:50%;
-  opacity:1;
+  background: rgba(77, 124, 254, 1);
+  border-radius: 50%;
+  opacity: 1;
 }
-.body_one_down{
+.body_one_down {
   display: -webkit-flex;
   display: flex;
   flex-direction: row;
   flex-wrap: nowrap;
 }
-.body_one_down_left{
+.body_one_down_left {
   display: -webkit-flex;
   display: flex;
   flex-direction: row;
@@ -261,14 +270,14 @@ import { getUserInfo , getLicense ,findUserCount ,getCurApp ,getDisableAppId} fr
   flex-direction: row;
   flex-wrap: nowrap;
 }
-.model_one{
-  width:175px;
-  height:108px;
+.model_one {
+  width: 175px;
+  height: 108px;
   line-height: 108px;
-  background:rgba(255,255,255,1);
-  border:1px solid rgba(0,0,0,0.10196078431372549);
-  opacity:1;
-  border-radius:2px;
+  background: rgba(255, 255, 255, 1);
+  border: 1px solid rgba(0, 0, 0, 0.10196078431372549);
+  opacity: 1;
+  border-radius: 2px;
 }
 .models2 {
   width: 250px;
@@ -283,27 +292,27 @@ import { getUserInfo , getLicense ,findUserCount ,getCurApp ,getDisableAppId} fr
   flex-direction: row;
   flex-wrap: nowrap;
 }
-.height-50{
-  width:75px;
-  height:56px;
-  background:rgba(255,255,255,1);
-  border:1px solid rgba(0,0,0,0.10196078431372549);
-  opacity:1;
-  border-radius:2px;
+.height-50 {
+  width: 75px;
+  height: 56px;
+  background: rgba(255, 255, 255, 1);
+  border: 1px solid rgba(0, 0, 0, 0.10196078431372549);
+  opacity: 1;
+  border-radius: 2px;
 }
-.main1{
+.main1 {
   height: 800px;
   width: 1599px;
-  MARGIN-RIGHT: auto;
-  MARGIN-LEFT: auto;
+  margin-right: auto;
+  margin-left: auto;
 }
-.main{
-  overflow:auto;
+.main {
+  overflow: auto;
 }
 </style>
 
 <style>
-.ant-progress-circle .ant-progress-text{
-  font-size:15px;
+.ant-progress-circle .ant-progress-text {
+  font-size: 15px;
 }
 </style>
--- src/views/system/personalSettings/page/LoggingStatements.vue
+++ src/views/system/personalSettings/page/LoggingStatements.vue
@@ -13,35 +13,35 @@
         <!--头像点击展开后的页面 -->
         <div v-if="isOut1">
           <div class="trueName" style="display:flex;align-items: center;">
-            真实姓名:&nbsp;&nbsp;<a-input  size="small" style="width:200px;height:32px;"/>
-            &nbsp;
-            <a-icon type="edit" style="color:#336CFB;font-size:20px;" @click="edit"/>
+            真实姓名:&nbsp;&nbsp;
+            <a-input size="small" style="width:200px;height:32px;" />&nbsp;
+            <a-icon type="edit" style="color:#336CFB;font-size:20px;" />
           </div>
           <template>
-            <system-upload  @success="onUploadSuccess" :url="imgUrl" @del="onDelImg"></system-upload>
+            <system-upload @success="onUploadSuccess" :url="imgUrl" @del="onDelImg"></system-upload>
           </template>
           <p style="font-size:12px;color:#ccc;">点击上传图片,建议上传22X22,支持svg、png、jpg格式,限制5M内</p>
           <div class="pic" style="width:360px;height:92px;">
             <!-- <img src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1651287656,2337091096&fm=26&gp=0.jpg" 
             alt="111" 
             style="height:80px;width:80px;">
-            彭于晏 -->
+            彭于晏-->
           </div>
         </div>
       </div>
 
       <!-- 用户名 -->
-      <div class="total">
+      <!-- <div class="total">
         <div class="modle">
           <div class="title">用户名</div>
           <div class="anse">修改你的登陆用户名和真实姓名,用户名只能是英文、数字和下划线组成,且必须以字母开头。</div>
           <div class="btn">
             <a-button type="primary" @click="expend(2)">{{param2}}</a-button>
           </div>
-        </div>
+      </div>-->
 
-        <!-- 用户名点击展开后的页面 -->
-        <div v-if="isOut2">
+      <!-- 用户名点击展开后的页面 -->
+      <!-- <div v-if="isOut2">
           <div>
             <a-form
             :model="formData"
@@ -58,19 +58,19 @@
             </a-form>
           </div>
         </div>
-      </div>
+      </div>-->
 
       <!-- 修改密码 -->
-      <div class="total">
+      <!-- <div class="total">
         <div class="modle">
           <div class="title">修改密码</div>
           <div class="anse">修改密码时需要输入当前密码,如果你忘记了当前密码,可以点击这里通过手机短信重置你的密码</div>
           <div class="btn">
             <a-button type="primary" @click="expend(3)">{{param3}}</a-button>
           </div>
-        </div>
-        <!-- 修改密码点击展开后的页面 -->
-        <div v-if="isOut3">
+      </div>-->
+      <!-- 修改密码点击展开后的页面 -->
+      <!-- <div v-if="isOut3">
           <div>
             <a-form
             :model="formData"
@@ -87,19 +87,19 @@
             </a-form>
           </div>
         </div>
-      </div>
-      
+      </div>-->
+
       <!-- 修改联系方式 -->
-      <div class="total">
+      <!-- <div class="total">
         <div class="modle">
           <div class="title">修改联系方式</div>
           <div class="anse">手机和邮箱时系统会发送验证码到新的手机号码上,然后输入验证码后完成修改。</div>
           <div class="btn">
             <a-button type="primary" @click="expend(4)">{{param4}}</a-button>
           </div>
-        </div>
-        <!-- 用户名点击展开后的页面 -->
-        <div v-if="isOut4">
+      </div>-->
+      <!-- 用户名点击展开后的页面 -->
+      <!-- <div v-if="isOut4">
           <div>
             <div class="trueName" style="display:flex;align-items: center;">
               手机号:&nbsp;&nbsp;<a-input  size="small" style="width:200px;height:32px;"/>
@@ -113,100 +113,113 @@
             </div>
           </div>
         </div>
-      </div>
+      </div>-->
     </div>
   </div>
 </template>
 
 <script>
+import { getUserInfo } from '@/api/mylogin'
 import { getAllCompanyTree, updateCompany, deleteCompany } from '@/api/company'
 import SystemUpload from '../../enterpriseSettings/page/Module/SystemUpload'
 import typeUtils from '@/utils/typeUtils'
 import { minxinModal } from '@/utils/mixin.js'
-  export default {
-    name: 'LoggingStatements',
-    components: { SystemUpload },
-    data() {
-      return {
-        curSelectComId: undefined,
-        imgUrl: '',
-        isOut1:false,
-        isOut2:false,
-        isOut3:false,
-        isOut4:false,
-        param1:'展开+',
-        param2:'展开+',
-        param3:'展开+',
-        param4:'展开+',
-        formData:{
-          text1:'',
-          text2:''
-        },
-        formRules: {
-          text1: [
-            {required: true, message: '请输入名称', trigger: 'blur'},
-            {min: 1, max: 32, message: '支持汉字、字母、数值任意组合，限32个字符', trigger: 'blur'}
-          ],
-          text2: [
-            {required: true, message: '请输入名称', trigger: 'blur'},
-            {min: 1, max: 32, message: '支持汉字、字母、数值任意组合，限32个字符', trigger: 'blur'}
-          ]
+export default {
+  name: 'LoggingStatements',
+  components: { SystemUpload },
+  data () {
+    return {
+      curSelectComId: undefined,
+      userId: '',//用户的id
+      imgUrl: '',
+      isOut1: false,
+      // isOut2: false,
+      // isOut3: false,
+      // isOut4: false,
+      param1: '展开+',
+      // param2: '展开+',
+      // param3: '展开+',
+      // param4: '展开+',
+      formData: {
+        text1: '',
+        text2: ''
+      },
+      formRules: {
+        text1: [
+          { required: true, message: '请输入名称', trigger: 'blur' },
+          { min: 1, max: 32, message: '支持汉字、字母、数值任意组合，限32个字符', trigger: 'blur' }
+        ],
+        text2: [
+          { required: true, message: '请输入名称', trigger: 'blur' },
+          { min: 1, max: 32, message: '支持汉字、字母、数值任意组合，限32个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  created () {
+    // this.reloadCompany();
+    this.getUser_Info();
+  },
+  mixins: [minxinModal],
+  methods: {
+    getUser_Info () {
+      debugger
+      let me = this;
+      let userId
+      getUserInfo().then(function (res) {
+        if (res.code === 200 && res.data) {
+          me.userId = res.data.id
+        }
+      })
+    },
+    expend (val) {
+      //展开按钮的点击事件
+      if (val == 1) {
+        if (this.param1 == '展开+') {
+          this.isOut1 = true;
+          this.param1 = '收起...';
+        } else {
+          this.isOut1 = false;
+          this.param1 = '展开+';
+        }
+      } else if (val == 2) {
+        if (this.param2 == '展开+') {
+          this.isOut2 = true;
+          this.param2 = '收起...';
+        } else {
+          this.isOut2 = false;
+          this.param2 = '展开+';
+        }
+      } else if (val == 3) {
+        if (this.param3 == '展开+') {
+          this.isOut3 = true;
+          this.param3 = '收起...';
+        } else {
+          this.isOut3 = false;
+          this.param3 = '展开+';
+        }
+      } else {
+        if (this.param4 == '展开+') {
+          this.isOut4 = true;
+          this.param4 = '收起...';
+        } else {
+          this.isOut4 = false;
+          this.param4 = '展开+';
         }
       }
     },
-    created() {
-      this.reloadCompany();
-    },
-    mixins: [minxinModal],
-    methods: {
-      expend(val){
-        debugger
-        //展开按钮的点击事件
-        if(val == 1){
-          if(this.param1 == '展开+'){
-          this.isOut1 = true;
-          this.param1 = '收起...';
-          }else{
-            this.isOut1 = false;
-            this.param1 = '展开+';
-          }
-        }else if(val == 2){
-          if(this.param2 == '展开+'){
-          this.isOut2 = true;
-          this.param2 = '收起...';
-          }else{
-            this.isOut2 = false;
-            this.param2 = '展开+';
-          }
-        }else if(val == 3){
-          if(this.param3 == '展开+'){
-          this.isOut3 = true;
-          this.param3 = '收起...';
-          }else{
-            this.isOut3 = false;
-            this.param3 = '展开+';
-          }
-        }else{
-          if(this.param4 == '展开+'){
-          this.isOut4 = true;
-          this.param4 = '收起...';
-          }else{
-            this.isOut4 = false;
-            this.param4 = '展开+';
-          }
-        } 
-      },
-      onDelImg: function () {
+    onDelImg: function () {
+      debugger
       const _this = this
       this.confirm({
         title: '确认删除该图片吗吗',
         content: '',
         onOk: function () {
-          updateCompany({ id: _this.curSelectComId, avatar: 0 }).then(function (res) {
+          updateCompany({ id: _this.userId, avatar: 0 }).then(function (res) {
             if (res.code === 200) {
               _this.$message.success('删除成功')
               _this.imgUrl = ''
-              _this.reloadCompany()
+              // _this.reloadCompany()
             } else {
               _this.$message.error('删除失败')
             }
@@ -216,79 +229,84 @@ import { minxinModal } from '@/utils/mixin.js'
     },
     onUploadSuccess: function (data) {
       debugger
+      let userId = this.userId
+      if (!userId) return
       const _this = this
-      updateCompany({ id: this.curSelectComId, avatar: data.id }).then(function (res) {
+      updateCompany({ id: userId, avatar: data.id }).then(function (res) {
         if (res.code === 200) {
           _this.imgUrl = data.thumbUrl
           _this.$message.success('上传成功')
-          _this.reloadCompany()
+          // _this.reloadCompany() 
         } else {
           _this.$message.error('上传失败')
         }
       })
+
+
     },
-    reloadCompany: function () {
-      const _this = this
-      getAllCompanyTree().then(function (treeData) {
-        _this.comTreeData = treeData
-      })
-    },
-    }
+    // reloadCompany: function () {
+    //   debugger
+    //   const _this = this
+    //   getAllCompanyTree().then(function (treeData) {
+    //     _this.comTreeData = treeData
+    //   })
+    // },
   }
+}
 </script>
 
 <style scoped>
-.main{
+.main {
   overflow: auto;
 }
-.loginInfo{
+.loginInfo {
   height: 800px;
   width: 1599px;
-  MARGIN-RIGHT: auto;
-  MARGIN-LEFT: auto;
+  margin-right: auto;
+  margin-left: auto;
 }
-.total{
+.total {
   border-bottom: 1px solid #ccc;
   padding-top: 20px;
 }
-.modle{
+.modle {
   display: -webkit-flex;
   display: flex;
   flex-direction: row;
   flex-wrap: nowrap;
-  height:65px; 
+  height: 65px;
 }
-.title{
-  width:196px;
-  font-size:16px;
-  font-family:Microsoft YaHei;
-  font-weight:400;
-  line-height:65px;
-  color:rgba(42,43,47,1);
-  opacity:1;
+.title {
+  width: 196px;
+  font-size: 16px;
+  font-family: Microsoft YaHei;
+  font-weight: 400;
+  line-height: 65px;
+  color: rgba(42, 43, 47, 1);
+  opacity: 1;
 }
-.anse{
-  width:1040px;
-  font-size:14px;
-  font-family:Microsoft YaHei;
-  font-weight:400;
-  line-height:70px;
-  color:rgba(42,43,47,1);
-  opacity:0.5;
+.anse {
+  width: 1040px;
+  font-size: 14px;
+  font-family: Microsoft YaHei;
+  font-weight: 400;
+  line-height: 70px;
+  color: rgba(42, 43, 47, 1);
+  opacity: 0.5;
 }
-.btn{
-  line-height:65px;
-  padding-left:800px;
+.btn {
+  line-height: 65px;
+  padding-left: 800px;
 }
-.pic{
+.pic {
   padding-bottom: 10px;
 }
-.trueName{
+.trueName {
   font-size: 14px;
   line-height: 22px;
   padding-bottom: 32px;
 }
-.pwd{
+.pwd {
   padding-left: 13px;
 }
 </style>
