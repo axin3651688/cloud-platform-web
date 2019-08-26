72239360dcf518ed44b399665a7ba9452cd27f2a
fangqiang <fangqiang@cnbisoft.com>
2019-08-26 09:03:02 +0800

    修改token刷新时间

--- src/config/projectSetting.js
+++ src/config/projectSetting.js
@@ -2,5 +2,5 @@ export const TIME_SETTING = {
   // 小于此时间登陆刷新
   reLoginTime: 3 * 3600,
   // 每隔此时间刷新token， 注意 refreshTokenInterval < reLoginTime
-  refreshTokenInterval: 7200
+  refreshTokenInterval: 7200 * 1000
 }
