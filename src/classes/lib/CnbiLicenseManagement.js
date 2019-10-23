d339346ee9a65e0da999b76666a4e63378c4c2ef
shj <shj@cnbisoft.com>
2019-10-23 08:44:23 +0800

    冲突

--- src/classes/lib/CnbiLicenseManagement.js
+++ src/classes/lib/CnbiLicenseManagement.js
@@ -6,20 +6,22 @@ import {
   SAVE_LICENSE,
   DELETE_LICENSE,
   UPDATE_LICENSE,
-  UPDATE_VAIL_LICENSE
+  UPDATE_VAIL_LICENSE,
+  GET_RESOURCES_CARD,
+  GET_RESOURCES_MODULE
 } from '@/api/productModule'
 /**
-* @desc    : 该类为牌照管理类，为了实现页面代码更加简单清晰而写的，
-             其中有增加牌照，删除牌照，编辑牌照，查询牌照等其他操作，
-             有租户的属性，在页面代码中只需引用该类，并创建一个牌照
-             对象，以对象点的形式去调用需要的属性和方法，也不再需要
-             在组件中引用接口等，也便于后期维护......
-* @author  : zj
-* @date  : 2019/10/14
-* @param   {}
-* @return  {}
-* @update   by
-*/
+ * @desc    : 该类为牌照管理类，为了实现页面代码更加简单清晰而写的，
+ 其中有增加牌照，删除牌照，编辑牌照，查询牌照等其他操作，
+ 有租户的属性，在页面代码中只需引用该类，并创建一个牌照
+ 对象，以对象点的形式去调用需要的属性和方法，也不再需要
+ 在组件中引用接口等，也便于后期维护......
+ * @author  : zj
+ * @date  : 2019/10/14
+ * @param   {}
+ * @return  {}
+ * @update   by
+ */
 class CnbiLicenseManagement {
   constructor(obj) {
     Object.assign(this, obj)
@@ -175,13 +177,13 @@ class CnbiLicenseManagement {
     return res.data
   }
   /**
-  * @desc    : 查询模块列表
-  * @author  : zj
-  * @date  : 2019/10/22
-  * @param   {}
-  * @return  {}
-  * @update   by
-  */
+   * @desc    : 查询模块列表
+   * @author  : zj
+   * @date  : 2019/10/22
+   * @param   {}
+   * @return  {}
+   * @update   by
+   */
   async getResourcesModule() {
     let res = await GET_RESOURCES_MODULE();
     return res.data
