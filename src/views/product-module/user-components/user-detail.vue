103b1e53ae8a0f2ca886693c75fd9c98750b3e6b
zj <zj@cnbisoft.com>
2019-10-29 17:21:01 +0800

    用户管理 的列表查询   模糊查询  添加  授权

--- src/views/product-module/user-components/user-detail.vue
+++ src/views/product-module/user-components/user-detail.vue
@@ -2,17 +2,21 @@
   <div class="user-detail">
     <a-row>
       <!--左侧-->
-      <a-col :span="6" style="overflow: hidden;border-bottom: 1px solid #EAEDF3;">
+      <a-col :span="6"
+             style="overflow: hidden;border-bottom: 1px solid #EAEDF3;">
 
         <div style="display: flex;flex-direction: column;border-bottom: 1px solid #EAEDF3;">
           <div style="display: flex;flex-direction: column;justify-content: center;align-items: center">
-            <img src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1163926984,507042275&fm=26&gp=0.jpg"/>
+            <img :src="info.avatarUrl" />
           </div>
           <div style="padding-left: 32px;margin-top: 20px;">
             <h2>租户名称</h2>
-            <a-select style="width: 120px;" placeholder="租户的状态" @change="handleChange">
-              <template slot="suffixIcon" >
-                <img style="width: 12px;" src="../../../assets/icons/paixu.svg"/>
+            <a-select style="width: 120px;"
+                      placeholder="租户的状态"
+                      @change="handleChange">
+              <template slot="suffixIcon">
+                <img style="width: 12px;"
+                     src="../../../assets/icons/paixu.svg" />
               </template>
               <a-select-option value="male">
                 male
@@ -32,44 +36,50 @@
         <!--绑定账号-->
         <div style="margin-top: 20px;">
           <div style="margin-left: 32px"><span style="color: #9EA0A5">绑定账号</span></div>
-          <account style="cursor: pointer" class="account" :name="'QQ'" :number="1234567890123"></account>
-          <account style="cursor: pointer" class="account" :name="'微信'" :number="12345"></account>
+          <account style="cursor: pointer"
+                   class="account"
+                   :name="'QQ'"
+                   :number="1234567890123"></account>
+          <account style="cursor: pointer"
+                   class="account"
+                   :name="'微信'"
+                   :number="12345"></account>
         </div>
       </a-col>
-      <a-col :span="3" style="overflow: hidden;border-bottom: 1px solid #EAEDF3;">
+      <a-col :span="3"
+             style="overflow: hidden;border-bottom: 1px solid #EAEDF3;">
       </a-col>
       <!--右侧-->
-      <a-col :span="12" >
-        <a-form :form="form" class="user-table">
-          <a-row
-            :gutter="24"
-            class="row1">
+      <a-col :span="12">
+        <a-form :form="form"
+                class="user-table">
+          <a-row :gutter="24"
+                 class="row1">
             <a-col :span="12">
               <a-form-item label="名称">
-                <span v-if="!InfoState">狗剩</span>
-                <a-input
-                  v-else
-                  placeholder="请输入名称"
-                  v-decorator="['a',{rules: [{ required: true, message: '名称不能为空!' }],}]" />
+                <span v-if="!InfoState">{{info.nickname}}</span>
+                <a-input v-else
+                         placeholder="请输入名称"
+                         v-decorator="['a',{rules: [{ required: true, message: '名称不能为空!' }],}]" />
               </a-form-item>
             </a-col>
             <a-col :span="12">
               <a-form-item label="用户名">
-                <span v-if="!InfoState">狗蛋</span>
-                <a-input
-                  v-else
-                  placeholder="请输入用户名"
-                  v-decorator="['b',{rules: [{ required: true, message: '地址不能为空!' }],}]" />
+                <span v-if="!InfoState">{{info.username}}</span>
+                <a-input v-else
+                         placeholder="请输入用户名"
+                         v-decorator="['b',{rules: [{ required: true, message: '地址不能为空!' }],}]" />
               </a-form-item>
             </a-col>
           </a-row>
-          <a-row
-            :gutter="24"
-            class="row1">
+          <a-row :gutter="24"
+                 class="row1">
             <a-col :span="12">
               <a-form-item label="性别">
-                <span v-if="!InfoState">公</span>
-                <a-radio-group name="c" :defaultValue="1" v-else>
+                <span v-if="!InfoState">{{info.sex}}</span>
+                <a-radio-group name="c"
+                               :defaultValue="1"
+                               v-else>
                   <a-radio :value="1">男</a-radio>
                   <a-radio :value="2">女</a-radio>
                 </a-radio-group>
@@ -77,94 +87,86 @@
             </a-col>
             <a-col :span="12">
               <a-form-item label="创建日期">
-                <span v-if="!InfoState">2020.12.36</span>
-                <a-input
-                  v-else
-                  disabled
-                />
+                <span v-if="!InfoState">{{info.createTime}}</span>
+                <a-input v-else
+                         disabled />
               </a-form-item>
             </a-col>
           </a-row>
-          <a-row
-            :gutter="24"
-            class="row1">
+          <a-row :gutter="24"
+                 class="row1">
             <a-col :span="12">
               <a-form-item label="密码">
                 <span v-if="!InfoState">*******</span>
-                <a-input
-                  v-else
-                  disabled
-                />
+                <a-input v-else
+                         disabled />
               </a-form-item>
             </a-col>
             <a-col :span="12">
               <a-form-item label="创建人">
-                <span v-if="!InfoState">哈哈哈</span>
-                <a-input
-                  v-else
-                  disabled
-                />
+                <span v-if="!InfoState">{{info.createUserName}}</span>
+                <a-input v-else
+                         disabled />
               </a-form-item>
             </a-col>
           </a-row>
-          <a-row
-            :gutter="24"
-            class="row1">
+          <a-row :gutter="24"
+                 class="row1">
             <a-col :span="12">
               <a-form-item label="手机号">
-                <span v-if="!InfoState">0101001</span>
-                <a-input
-                  v-else
-                  placeholder="请输入手机号"
-                  v-decorator="['phone',{rules: [{ required: true, message: 'Please input your phone number!' }],}]"/>
+                <span v-if="!InfoState">{{info.phone}}</span>
+                <a-input v-else
+                         placeholder="请输入手机号"
+                         v-decorator="['phone',{rules: [{ required: true, message: 'Please input your phone number!' }],}]" />
 
               </a-form-item>
             </a-col>
             <a-col :span="12">
               <a-form-item label="最近更新日期">
-                <span v-if="!InfoState">2020.2.53</span>
-                <a-input
-                  v-else
-                  disabled
-                />
+                <span v-if="!InfoState">{{info.updateTime}}</span>
+                <a-input v-else
+                         disabled />
               </a-form-item>
             </a-col>
           </a-row>
-          <a-row
-            :gutter="24"
-            class="row1">
+          <a-row :gutter="24"
+                 class="row1">
             <a-col :span="12">
 
               <a-form-item label="邮箱">
-                <span v-if="!InfoState">啊发发。发烧饭</span>
-                <a-input
-                  v-else
-                  placeholder="请输入邮箱"
-                  v-decorator="['g',{rules: [{ required: true,pattern: /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/, message: '邮箱不能为空!' }],}]" />
+                <span v-if="!InfoState">{{info.email}}</span>
+                <a-input v-else
+                         placeholder="请输入邮箱"
+                         v-decorator="['g',{rules: [{ required: true,pattern: /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/, message: '邮箱不能为空!' }],}]" />
               </a-form-item>
             </a-col>
             <a-col :span="12">
               <a-form-item label="最近更新人">
-                <span v-if="!InfoState">啊深地税发</span>
-                <a-input
-                  v-else
-                  disabled
-                />
+                <span v-if="!InfoState">{{info.updateUserName}}</span>
+                <a-input v-else
+                         disabled />
               </a-form-item>
             </a-col>
           </a-row>
-          <a-row
-            :gutter="24"
-            class="row1">
-            <a-col :span="12" v-if="!InfoState">
-              <a-form-item >
-                <a-button type="primary" @click="InfoState=true"><a-icon type="cloud-upload" />修改</a-button>
-              </a-form-item >
+          <a-row :gutter="24"
+                 class="row1">
+            <a-col :span="12"
+                   v-if="!InfoState">
+              <a-form-item>
+                <a-button type="primary"
+                          @click="InfoState=true">
+                  <a-icon type="cloud-upload" />修改</a-button>
+              </a-form-item>
             </a-col>
-            <a-col :span="12" v-else>
-              <a-form-item >
-                <a-button type="primary" style="margin-right: 20px" @click="cancelEdit">取消</a-button>
-                <a-button type="primary" @click="saveEdit"><a-icon type="cloud-upload" />保存</a-button>
+            <a-col :span="12"
+                   v-else>
+              <a-form-item>
+                <a-button type="primary"
+                          style="margin-right: 20px"
+                          @click="cancelEdit">取消</a-button>
+                <a-button type="primary"
+                          @click="saveEdit">
+                  <a-icon type="cloud-upload" />保存</a-button>
               </a-form-item>
             </a-col>
           </a-row>
@@ -175,6 +177,7 @@
 </template>
 
 <script>
+import CnbiUserManagement from '@/classes/lib/CnbiUserManagement'
 import Account from './account'
 import AFormItem from 'ant-design-vue/es/form/FormItem'
 export default {
@@ -185,11 +188,30 @@ export default {
   },
   data () {
     return {
+      UserMObj: null,
       form: this.$form.createForm(this),
-      InfoState: false
+      InfoState: false,
+      detailId: null, // 点击的用户详情对应的id
+      info: null, // 当前用户的所有详细信息
     }
   },
+  created () {
+    this.UserMObj = new CnbiUserManagement()
+    this.detailId = this.$route.query.id.id
+    this.getData()
+  },
   methods: {
+    //进入页面  获取该用户的详细信息
+    async getData () {
+      const data = await this.UserMObj.findUserDetailInfo(this.detailId);
+      data.data.createTime = new Date(data.data.createTime * 1).toLocaleString()
+      data.data.updateTime = new Date(data.data.updateTime * 1).toLocaleString()
+      //info.bindApps是一个数组，里面有对应账户的图标名称等信息
+      this.info = data.data
+      console.log(data.data, '877878787878');
+
+    },
+
     handleChange (val) {
 
     },
@@ -205,14 +227,14 @@ export default {
 </script>
 
 <style scoped>
-  .user-table input{
-    width: auto;
-  }
-  .user-table{
-    max-width: 500px;
-  }
-  .user-detail{
-    background-color: #fff;
-    padding: 16px 32px;
-  }
+.user-table input {
+  width: auto;
+}
+.user-table {
+  max-width: 500px;
+}
+.user-detail {
+  background-color: #fff;
+  padding: 16px 32px;
+}
 </style>
